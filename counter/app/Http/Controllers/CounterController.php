7261a2899a15cad22336ab6dda4bee66b6119234
BAGArt <baltaev.artur@gmail.com>
2017-06-27 16:02:50 +0300

    get counter

--- counter/app/Http/Controllers/CounterController.php
+++ counter/app/Http/Controllers/CounterController.php
@@ -7,18 +7,49 @@ use App\Repositories\CounterRepository;
 use App\Repositories\CountryRepository;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class CounterController extends Controller
 {
     public function get(Request $request)
     {
-        $format = $request->get('format') ?? 'json';
-        dd($format);
-        $result = (new CounterRepository)->getLastTopEventSumByCountry();
-        //@todo output format
-        return $result;
+        $counters = (new CounterRepository)
+            ->getLastTopEventSumByCountry();
+        switch ($request->get('format') ?? 'json') {
+            case 'csv':
+                return $this->exportCsv($counters);
+            case 'json':
+                return response()->json($counters);
+            default:
+                throw new \HttpRequestException('format not allowed');
+        }
     }
 
+    public function exportCsv($counters)
+    {
+        $headers = array(
+            "Content-type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=counter.csv",
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0"
+        );
+
+        $callback = function() use ($counters)
+        {
+            if ($counters) {
+                $file = fopen('php://output', 'w');
+                fputcsv($file, array_keys((array)$counters[0]));
+                foreach ($counters as $counter) {
+                    fputcsv($file, (array)$counter);
+                }
+                fclose($file);
+            }
+        };
+
+        return StreamedResponse::create($callback, 200, $headers);
+    }
+    
     public function inc(Request $request)
     {
         if (
--- counter/tests/ControllerCounterGetTest.php
+++ counter/tests/ControllerCounterGetTest.php
@@ -1,26 +1,42 @@
 <?php
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+
 class ControllerCounterGetTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    public function testGet()
+    {
+        $this->get('/counter');
+        $this->assertEquals(200, $this->response->getStatusCode());
+        $this->assertEquals('application/json', $this->response->headers->get('content-type'));
+    }
+
     public function testGetJson()
     {
         $this->get('/counter?format=json');
-        dd($this->response->getContent());
-        $this->assertEquals(
-            $this->app->version(),
-            $this->response->getContent()
-        );
+
+        $this->assertEquals(200, $this->response->getStatusCode());
+        $this->assertEquals('application/json', $this->response->headers->get('content-type'));
+        $this->assertNotEmpty($this->response->getContent());
+        $this->assertStringStartsWith('[{', $this->response->getContent());
+        $this->assertStringEndsWith('}]', $this->response->getContent());
+        $json = json_decode($this->response->getContent(), true);
+        $this->assertNotEmpty($json);
+        $this->assertArrayHasKey('country', $json[0]);
+        $this->assertArrayHasKey('event', $json[0]);
+        $this->assertArrayHasKey('counter', $json[0]);
+
     }
 
     public function testGetCsv()
     {
-        //@todo
         $this->get('/counter?format=csv');
+        $this->assertEquals(200, $this->response->getStatusCode());
+        $this->assertEquals('text/csv', $this->response->headers->get('content-type'));
+        ob_start();
+        $this->response->sendContent();
+        $result = ob_get_clean();
+
+        $this->assertGreaterThan(0, mb_strlen($result));
     }
 }
